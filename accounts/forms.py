8cead5abb73f0794cc50e6aef4395f9e06b4a0ea
aliahmadi <aliahmadiaaa0722@gmail.com>
2024-06-25 11:45:47 +0330

    add validerror form clean_()

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+
 
 
 class UserRegistrationForm(forms.Form):
@@ -9,4 +14,27 @@ class UserRegistrationForm(forms.Form):
 
     username = forms.CharField()
     email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('this username already exists')
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email already exists')
+        return user
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('password must match')
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+
     def get(self, request):
-        form = UserRegistrationForm
-        return render(request, 'accounts/register.html', {'form': form})
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        else:
+            return render(request, self.template_name, {'form': form})
