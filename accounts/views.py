dc8c06e04fe5dbdf458b2247b5ca1383aa81251e
aliahmadi <aliahmadiaaa0722@gmail.com>
2024-06-25 14:42:14 +0330

    add page login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from django.contrib.auth import login, logout, authenticate
 
 class UserRegisterView(View):
     form_class = UserRegistrationForm
@@ -22,3 +22,32 @@ class UserRegisterView(View):
             return redirect('home:home')
         else:
             return render(request, self.template_name, {'form': form})
+
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('home:home')
+            else:
+                messages.error(request, 'username or password in wrong', 'error')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request,'you logged out successfully', 'success')
+        return redirect('home:home')
