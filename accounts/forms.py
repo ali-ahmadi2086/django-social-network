f25e03c7328ff9511fb739ac23bc8e0a841fd0c3
aliahmadi <aliahmadiaaa0722@gmail.com>
2024-06-25 10:38:02 +0330

    add widget form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for item in UserRegistrationForm.visible_fields(self):
+            item.field.widget.attrs['class'] = 'form-control'
+
     username = forms.CharField()
     email = forms.EmailField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
